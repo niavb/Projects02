e6bf60272e45cf350f5dd515a5444e202fd2c8cf
Nia Bakova <69983201+niavb@users.noreply.github.com>
2021-05-17 10:11:24 +0300

    Add files via upload

---
+++ Projects15/MazeControll.java
@@ -0,0 +1,45 @@
+package projects15;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class MazeControll extends JPanel {
+	private MazePanel mazePanel = new MazePanel();
+	private JButton findPathBtn = new JButton("Find Path");
+	private JButton cleanPathBtn = new JButton("Reset");
+	
+	public MazeControll(){
+		setLayout(new BorderLayout());
+		add(mazePanel, BorderLayout.CENTER);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(findPathBtn);
+		buttonPanel.add(cleanPathBtn);
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		findPathBtn.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				mazePanel.findPath();
+			}
+			
+		});
+		
+		cleanPathBtn.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				mazePanel.cleanPath;
+			}
+			
+		});
+		
+	}
+	
+	
+
+}
---
+++ Projects15/MazePanel.java
@@ -0,0 +1,26 @@
+package projects15;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class MazePanel extends JPanel{
+	boolean displayPath = false;
+	
+	public MazePanel(){
+		
+	}
+	
+	public void findPath(){
+		
+	}
+	
+	public void cleanPath(){
+		
+	}
+	
+	protected void printComponent(Graphics g){
+		super.printComponent(g);
+	}
+
+}
